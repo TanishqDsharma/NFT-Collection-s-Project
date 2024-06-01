3df9746bdc970578bbc20594c374d66dd25d273b
TanishqDsharma <tanishqsharma612@gmail.com>
2024-06-01 17:14:21 +0000

    Added minting function and tokenURI function

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -8,13 +8,20 @@ import {ERC721} from "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC72
 contract  BasicNft is ERC721 {
 
     uint256 private s_tokenCounter;
-    
+    mapping (uint256=>string) private s_tokenIdtoUri;
     constructor() ERC721("Dogie","Dog"){
         s_tokenCounter =0;
     }
 
+    function mintNft(string memory tokenUri) public {
+        s_tokenIdtoUri[s_tokenCounter] = tokenUri;
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+
+    }
+
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        
+        return s_tokenIdtoUri[s_tokenCounter];
     }
 
     
