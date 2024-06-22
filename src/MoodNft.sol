e2bb953c5a8739e02afdf9ce94f740fc84882cc7
TanishqDsharma <tanishqsharma612@gmail.com>
2024-06-22 17:46:42 +0000

    Added Function and comments

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -1,5 +1,4 @@
-//SPDX-License-Identifier: MIT
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
 import {ERC721} from "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
@@ -7,64 +6,85 @@ import {Base64} from "../lib/openzeppelin-contracts/contracts/utils/Base64.sol";
 
 contract MoodNft is ERC721{
 
-    /** ERRORS */
-    error MoodNft__CantFlipMoodIfNotOwner();
-
+    //This will keep track of token Id generated
     uint256 private s_tokenCounter;
-    string private s_sadSvgImageUri;
-    string private s_happySvgImageUri;
 
-    enum Mood {
-        Happy,
-        Sad
+    string private s_sadSvgUri;
+    string private s_happySvgUri;
+
+    enum Mood{
+        HAPPY,
+        SAD
     }
+
+    //Mapping to map tokenId with the mood
     mapping(uint256=>Mood) private s_tokenIdToMood;
-
-    constructor(string memory sadSvg,string memory happySvg) ERC721("Mood NFT","MN"){
+    /**
+     * 
+     * @param sadSvgUri The image uri of the SadSvg 
+     * @param happySvgUri  The image uri of the HappySvg 
+     */
+    constructor(string memory sadSvgUri, string memory happySvgUri) ERC721("Mood NFT","MN") {
         s_tokenCounter=0;
-        s_happySvgImageUri = happySvg;
-        s_sadSvgImageUri = sadSvg;
-    }
-    
-
-    function mintNft() public  {
-        _safeMint(msg.sender, s_tokenCounter);
-        s_tokenIdToMood[s_tokenCounter] =Mood.Happy;
-        s_tokenCounter++;
-    }
-
-    function flipMood(uint256 tokenId) public {
-       if(msg.sender!=ownerOf(tokenId)){
-            revert MoodNft__CantFlipMoodIfNotOwner();
-       }
-       if(s_tokenIdToMood[tokenId]==Mood.Happy){
-        s_tokenIdToMood[tokenId]==Mood.Sad;
-       }else{
-        s_tokenIdToMood[tokenId]==Mood.Happy;
-
-       }
 
     }
 
+    /////////////////////////// 
+    //////Internal Function//// 
+    /////////////////////////// 
+
+    // This function returns the strings to baseURI string 
     function _baseURI() internal pure override returns(string memory){
         return "data:application/json;base64,";
     }
 
-    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-        
+
+
+    //////////////////////////
+    //public & external func//
+    /////////////////////////
+
+   
+    // This function will mint the NFT token
+    function mintNft() public {
+        _safeMint(msg.sender,s_tokenCounter);
+        //When an NFT get Minted default the NFT mood to HAPPY
+        s_tokenIdToMood[s_tokenCounter]=Mood.HAPPY;
+        s_tokenCounter++;
+    }
+
+    /**
+     * 
+     * @param tokenId The Id of the ERC721 token
+     * @notice This function returns the tokenURI of the ERC721 token, A token URI returns all the metadata related to the NFT.
+     */
+
+    function tokenURI(uint256 tokenId) public view override returns(string memory){
+
         string memory imageURI;
 
-        if(s_tokenIdToMood[s_tokenCounter]==Mood.Happy){
-            imageURI = s_happySvgImageUri;
+        if(s_tokenIdToMood[tokenId]==Mood.HAPPY){
+            imageURI = s_happySvgUri;
         }else{
-            imageURI = s_sadSvgImageUri;
+            imageURI = s_sadSvgUri;
         }
-        return string(abi.encodePacked(_baseURI(),Base64.encode(
-        bytes(abi.encodePacked(string.concat( '{"name":"',
-        name(), // You can add whatever name here
-        '", "description":"An NFT that reflects the mood of the owner, 100% on Chain!", ',
-        '"attributes": [{"trait_type": "moodiness", "value": 100}], "image":"',
-        imageURI,
-        '"}'))))));
+
+        return string(
+            abi.encodePacked(
+                _baseURI(),
+                Base64.encode(
+                    bytes( // bytes casting actually unnecessary as 'abi.encodePacked()' returns a bytes
+                        abi.encodePacked(
+                            '{"name":"',
+                            name(), // You can add whatever name here
+                            '", "description":"An NFT that reflects the mood of the owner, 100% on Chain!", ',
+                            '"attributes": [{"trait_type": "moodiness", "value": 100}], "image":"',
+                            imageURI,
+                            '"}'
+                        )
+                    )
+                )
+            )
+        );
     }
 }
