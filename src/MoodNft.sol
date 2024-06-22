51753b18de7a1c5a34bfd9d1d90870d61b0d6214
TanishqDsharma <tanishqsharma612@gmail.com>
2024-06-22 18:05:43 +0000

    Added flipMood Function

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -6,6 +6,12 @@ import {Base64} from "../lib/openzeppelin-contracts/contracts/utils/Base64.sol";
 
 contract MoodNft is ERC721{
 
+    ////////// 
+    //ERRORS//
+    ////////// 
+
+    error MoodNft_CantFlipIfNotOwner();
+
     //This will keep track of token Id generated
     uint256 private s_tokenCounter;
 
@@ -19,6 +25,7 @@ contract MoodNft is ERC721{
 
     //Mapping to map tokenId with the mood
     mapping(uint256=>Mood) private s_tokenIdToMood;
+
     /**
      * 
      * @param sadSvgUri The image uri of the SadSvg 
@@ -26,12 +33,14 @@ contract MoodNft is ERC721{
      */
     constructor(string memory sadSvgUri, string memory happySvgUri) ERC721("Mood NFT","MN") {
         s_tokenCounter=0;
+        s_sadSvgUri=sadSvgUri;
+        s_happySvgUri=happySvgUri;
 
     }
 
     /////////////////////////// 
     //////Internal Function//// 
-    /////////////////////////// 
+    /////////////////////////// S
 
     // This function returns the strings to baseURI string 
     function _baseURI() internal pure override returns(string memory){
@@ -52,6 +61,27 @@ contract MoodNft is ERC721{
         s_tokenIdToMood[s_tokenCounter]=Mood.HAPPY;
         s_tokenCounter++;
     }
+
+    /**
+     * 
+     * @param tokenId The Id of the ERC721 token
+     * @notice This function will flip the mood of the NFT for example if the mood is happy this function will change it sad
+     */
+
+     function flipmood(uint256 tokenId) public{
+        //Only want NFT owner to change the mood
+
+       if(!_isApprovedOrOwner(msg.sender,tokenId)){
+        revert MoodNft_CantFlipIfNotOwner();
+       }
+
+        if(s_tokenIdToMood[tokenId]==Mood.HAPPY){
+            s_tokenIdToMood[tokenId]==Mood.SAD;
+        }else{
+            s_tokenIdToMood[tokenId]==Mood.HAPPY;
+        }
+    }
+
 
     /**
      * 
