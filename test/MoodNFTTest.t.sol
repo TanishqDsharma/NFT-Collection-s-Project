eef072c7311d0bde49c93f310a1174d98c768d55
TanishqDsharma <tanishqsharma612@gmail.com>
2024-07-01 14:02:59 +0000

    More tests

--- test/MoodNFTTest.t.sol
+++ test/MoodNFTTest.t.sol
@@ -24,11 +24,7 @@ contract MoodNFTTest is Test{
         vm.deal(user,10e18);
     }
 
-    function () public {
-        vm.prank(user);
-        moodNft.mintNft();
-        console.log(moodNft.tokenURI(0));
-    }
+    
 
 
 }
